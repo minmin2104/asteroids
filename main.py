21e0dd94337b62c9717a15851f21e115497adc0d
minmin2104 <abeamin23@gmail.com>
2025-10-19 23:04:12 +0800

    unexpected behavior but it's cool

--- main.py
+++ main.py
@@ -41,8 +41,9 @@ class Main:
             if keys[pygame.K_RIGHT]:
                 ship.rotate(200 * self.dt)
             if keys[pygame.K_x]:
-                pass
+                ship.shoot()
 
+                ship.update(self.dt)
             ship.render(self.screen)
 
             pygame.display.flip()
--- ship.py
+++ ship.py
@@ -31,32 +31,49 @@ class Ship:
         ]
         self.color = color
         self.rect = None
-        self.angle = 0
+        self.direction = pygame.math.Vector2()
+        self.__angle = 0
+        self.__bullet_speed = 400
+        self.__bullets = []
 
     def rotate(self, rotation_speed):
-        self.angle += math.radians(rotation_speed)
+        self.__angle += math.radians(rotation_speed)
         for i, corner in enumerate(self.__base_corners.copy()):
             rotated_x = corner.x * \
-                math.cos(self.angle) - corner.y * math.sin(self.angle)
+                math.cos(self.__angle) - corner.y * math.sin(self.__angle)
             rotated_y = corner.x * \
-                math.sin(self.angle) + corner.y * math.cos(self.angle)
+                math.sin(self.__angle) + corner.y * math.cos(self.__angle)
             self.__corners[i].x = rotated_x + self.pos.x
             self.__corners[i].y = rotated_y + self.pos.y
 
+    def __get_direction(self):
+        self.direction = self.pos - self.__corners[0]
+        self.direction = self.direction.normalize()
+
     def move(self, speed):
-        direction = self.pos - self.__corners[0]
-        direction = direction.normalize()
+        self.__get_direction()
         speed = -speed
         for i, p in enumerate(self.__corners.copy()):
-            self.__corners[i].x += speed * direction.x
-            self.__corners[i].y += speed * direction.y
+            self.__corners[i].x += speed * self.direction.x
+            self.__corners[i].y += speed * self.direction.y
 
     def shoot(self):
-        pass
+        self.__bullets.append(
+            pygame.Rect(
+                self.__corners[0].x, self.__corners[0].y,
+                5, 5
+            ))
 
     def update(self, dt):
-        pass
+        self.__get_direction()
+        for bullet in self.__bullets:
+            bullet.x += (self.__bullet_speed *
+                         dt) * self.direction.x
+            bullet.y += (self.__bullet_speed *
+                         dt) * self.direction.y
 
     def render(self, screen):
         self.rect = pygame.draw.polygon(
             screen, self.color, self.__corners, self.width)
+        for bullet in self.__bullets:
+            pygame.draw.rect(screen, 'white', bullet)
