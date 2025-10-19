1daed7c86f010a368c62ad5a110614f2551abe51
minmin2104 <aminkafri0045@gmail.com>
2025-10-19 09:43:18 +0800

    Setting up pygame

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+
+
+class Main:
+    def __init__(self, win_title, win_width, win_height, fps):
+        pygame.init()
+        self.__win_width = win_width
+        self.__win_height = win_height
+        self.__win_title = win_title
+        self.screen = pygame.display.set_mode(
+            (self.__win_width, self.__win_height))
+        pygame.display.set_caption(self.__win_title)
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.fps = fps
+        self.game_loop()
+
+    def game_loop(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.screen.fill('purple')
+
+            pygame.display.flip()
+
+            self.clock.tick(self.fps)
+
+
+if __name__ == '__main__':
+    Main('Asteroid', 800, 600, 60)
