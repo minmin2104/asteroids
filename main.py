7665f05651ef68cdefeae93977299497bf09cfb9
minmin2104 <aminkafri0045@gmail.com>
2025-10-19 15:32:10 +0800

    rotate ship with left and right keys

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
 from ship import Ship
+import math
 import pygame
 
 
 class Main:
-    def __init__(self, win_title, win_width, win_height, fps):
+    def __init__(self, win_title, win_width, win_height, fps, screen_fill):
         pygame.init()
         self.__win_width = win_width
         self.__win_height = win_height
         self.__win_title = win_title
+        self.__screen_fill = screen_fill
         self.screen = pygame.display.set_mode(
             (self.__win_width, self.__win_height))
         pygame.display.set_caption(self.__win_title)
         self.clock = pygame.time.Clock()
         self.running = True
         self.fps = fps
-        self.game_loop()
+        self.dt = 0
+        self.main()
 
-    def game_loop(self):
+    def main(self):
 
         ship = Ship(
             pygame.math.Vector2(self.screen.get_width() / 2,
                                 self.screen.get_height() / 2),
             15,
             pygame.Color(255, 255, 255))
+
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            self.screen.fill('purple')
+            self.screen.fill(self.__screen_fill)
 
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                ship.rotate(-200 * self.dt)
+            if keys[pygame.K_RIGHT]:
+                ship.rotate(200 * self.dt)
+
+            ship.update(self.dt)
             ship.render(self.screen)
 
             pygame.display.flip()
 
-            self.clock.tick(self.fps)
+            self.dt = self.clock.tick(self.fps) / 1000
 
 
 if __name__ == '__main__':
-    Main('Asteroid', 800, 600, 60)
+    Main('Asteroid', 800, 600, 60, 'black')
--- ship.py
+++ ship.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 
 class Ship:
@@ -7,12 +8,33 @@ class Ship:
         self.width = width
         self.__center_distance = center_distance
         self.__corners = [
+            # The front
             pygame.math.Vector2(self.pos.x, self.pos.y - self.__center_distance),
+            # Left side
             pygame.math.Vector2(self.pos.x - self.__center_distance, self.pos.y + self.__center_distance),
+            # Right side
             pygame.math.Vector2(self.pos.x + self.__center_distance, self.pos.y + self.__center_distance),            
+        ]
+        self.__base_corners = [
+            pygame.math.Vector2(0, -self.__center_distance),
+            pygame.math.Vector2(-self.__center_distance, self.__center_distance),
+            pygame.math.Vector2(self.__center_distance, self.__center_distance),
         ]
         self.color = color
         self.rect = None
+        self.angle = 0
 
+    def rotate(self, rotation_speed):
+        self.angle += math.radians(rotation_speed)
+        for i, corner in enumerate(self.__base_corners.copy()):
+            rotated_x = corner.x * math.cos(self.angle) - corner.y * math.sin(self.angle)
+            rotated_y = corner.x * math.sin(self.angle) + corner.y * math.cos(self.angle)
+            self.__corners[i].x = rotated_x + self.pos.x
+            self.__corners[i].y = rotated_y + self.pos.y
+
+    def update(self, dt):
+        pass
+            
+        
     def render(self, screen):
         self.rect = pygame.draw.polygon(screen, self.color, self.__corners, self.width)
